984d586516071a4eab478fa7b50b9930453120ec
ahchang6 <ahchang6@d9c94b55-221a-4017-ad66-301122ff4019>
2017-02-27 19:40:51 +0000

    final upload

--- ahchang6/Assignment2.0/main.py
+++ ahchang6/Assignment2.0/main.py
@@ -4,14 +4,42 @@ import logging
 
 
 logging.basicConfig(filename='test.log', level=logging.DEBUG)
-test = Scraper('https://en.wikipedia.org/wiki/Ryan_Reynolds', 20)
-test.set_speed(1)
-graph = test.begin()
-print str(graph)
+running = True
+graph = Graph()
+while running:
+    print "Commands:"
+    print "0: quit"
+    print "1:(Actor/Movie to parse)"
+    print "2:(load graph path)"
+    print "3:(execute command)"
+    input = raw_input("Command")
+    print input
+    if input[0] == '0':
+        break
+    if input[0] == '1':
+        test = Scraper('https://en.wikipedia.org/wiki/' + input[2:], 30)
+        test.set_speed(1)
+        graph = test.begin()
+    if input[0] == '2':
+        graph.open_json(input[2:])
+    if input[0] == '3':
+        cmd = "print "
+        cmd += input[2:]
+        exec(cmd)
 
-for node in graph:
-    if node.is_actor:
-        print str(node.node_id) + " is actor"
+
+
+
+test_two = Scraper('https://en.wikipedia.org/wiki/Ryan_Reynolds', 30)
+test_two.set_speed(1)
+
+print str(Scraper.get_oldest_actors(graph, 5))
+
+print str(Scraper.get_movies(graph,2009))
+print str(Scraper.get_actors(graph,2009))
+
+print Scraper.actor_in(graph,"Chris Bender")
+print Scraper.movie_with(graph,"Blade: Trinity")
 
 
 
